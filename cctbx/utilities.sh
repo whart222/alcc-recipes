bafa40f3958ce0b13b734638b1a5dd418aff712e
Johannes Blaschke <jpblaschke@lbl.gov>
2021-02-26 15:28:45 -0800

    fix env setup script

--- cctbx/utilities.sh
+++ cctbx/utilities.sh
@@ -14,7 +14,10 @@ activate() {
 }
 
 
+
 mk-env () {
     micromamba activate
+    micromamba install python=3.8 -c defaults --yes
     micromamba create -f ${ROOT_PREFIX}/psana_environment.yml --yes
+    python ${ROOT_PREFIX}/opt/util/patch-rpath.py ${MAMBA_ROOT_PREFIX}/envs/psana_env/lib
 }
