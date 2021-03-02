b263c92cb19485f7d58c2e7aba5ed14f9e918ccb
Johannes Blaschke <jpblaschke@lbl.gov>
2021-03-01 21:13:46 -0800

    bump boostrap verison

---
+++ cctbx/update_boostrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# this () { echo $(readlink -f $(dirname ${BASH_SOURCE[0]})); }
+shopt -s expand_aliases
+alias this="readlink -f \$(dirname \${BASH_SOURCE[0]})"
+
+
+pushd $(this)
+rm bootstrap.py
+wget "https://raw.githubusercontent.com/cctbx/cctbx_project/master/libtbx/auto_build/bootstrap.py" --no-check-certificate
+popd
