2b673ab91aff3ee2aafcc7855604bf975ffcd23b
Johannes Blaschke <jpblaschke@lbl.gov>
2021-03-02 16:21:29 -0800

    fix mamba bootstrapping

--- cctbx/opt/get_mamba_linux-64.sh
+++ cctbx/opt/get_mamba_linux-64.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+
+# this () { echo $(readlink -f $(dirname ${BASH_SOURCE[0]})); }
+shopt -s expand_aliases
+alias this="readlink -f \$(dirname \${BASH_SOURCE[0]})"
+
+pushd $(this)
 wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+popd
--- cctbx/opt/get_mamba_linux-ppc64le.sh
+++ cctbx/opt/get_mamba_linux-ppc64le.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+
+# this () { echo $(readlink -f $(dirname ${BASH_SOURCE[0]})); }
+shopt -s expand_aliases
+alias this="readlink -f \$(dirname \${BASH_SOURCE[0]})"
+
+
+pushd $(this)
 wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-ppc64le/latest | tar -xvj bin/micromamba
+popd
