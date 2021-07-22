1b8aa02d31f52acfd4c590c625dbeafd88b549ce
Johannes Blaschke <jpblaschke@lbl.gov>
2021-07-22 14:30:21 -0700

    set SIT_DATA variable in activate.sh

--- cctbx/setup_cori.sh
+++ cctbx/setup_cori.sh
@@ -33,4 +33,7 @@ source ${ALCC_CCTBX_ROOT}/utilities.sh
 source ${ALCC_CCTBX_ROOT}/opt/site/nersc_cori.sh
 load-sysenv
 activate
+
+export SIT_DATA=/global/common/software/lcls/psdm/data:\$SIT_DATA
+export SIT_PSDM_DATA=/global/cscratch1/sd/psdatmgr/data/psdm
 EOF
