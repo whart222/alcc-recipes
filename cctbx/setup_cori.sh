f9738834cf061e3a1045c6b0a0b4be8fdf6bb06e
Felix Wittwer <40206209+Trzs@users.noreply.github.com>
2021-09-06 10:28:04 -0700

    Update setup_cori.sh

--- cctbx/setup_cori.sh
+++ cctbx/setup_cori.sh
@@ -7,7 +7,8 @@ export ALCC_CCTBX_ROOT=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 source ${ALCC_CCTBX_ROOT}/utilities.sh
 source ${ALCC_CCTBX_ROOT}/opt/util/fix_lib_nersc.sh
 source ${ALCC_CCTBX_ROOT}/opt/site/nersc_cori.sh
-source ${ALCC_CCTBX_ROOT}/update_bootstrap.sh
+
+${ALCC_CCTBX_ROOT}/update_bootstrap.sh
 
 fix-sysversions () {
     env-activate
