40b1600a99d5c9f7bc6b663230dbfa207aff0421
Johannes Blaschke <johannes.blaschke@gmail.com>
2021-09-07 09:48:57 -0700

    only remove files that exist

--- cctbx/update_bootstrap.sh
+++ cctbx/update_bootstrap.sh
@@ -6,6 +6,9 @@ alias this="readlink -f \$(dirname \${BASH_SOURCE[0]})"
 
 
 pushd $(this)
-rm bootstrap.py
+if [[ -f bootstrap.py ]]
+then
+    rm bootstrap.py
+fi
 wget "https://raw.githubusercontent.com/cctbx/cctbx_project/master/libtbx/auto_build/bootstrap.py" --no-check-certificate
 popd
