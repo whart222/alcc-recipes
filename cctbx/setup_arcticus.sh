362db9a9e263a020dad07ae114ec49302a73945d
William Hart <ac.wehart@jlselogin5.ftm.alcf.anl.gov>
2021-12-08 13:28:24 +0000

    Initial changes to script

--- cctbx/setup_arcticus.sh
+++ cctbx/setup_arcticus.sh
@@ -5,14 +5,14 @@ set -e
 export ALCC_CCTBX_ROOT=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 
 source ${ALCC_CCTBX_ROOT}/utilities.sh
-source ${ALCC_CCTBX_ROOT}/opt/util/fix_lib_alcf.sh
+#source ${ALCC_CCTBX_ROOT}/opt/util/fix_lib_alcf.sh
 source ${ALCC_CCTBX_ROOT}/opt/site/alcf_arcticus.sh
 
 ${ALCC_CCTBX_ROOT}/update_bootstrap.sh
 
 fix-sysversions () {
     env-activate
-    if fix-sysversions-perlmutter
+    if fix-sysversions-arcticus
     then
         return 1
     fi
@@ -22,12 +22,12 @@ ${ALCC_CCTBX_ROOT}/opt/get_mamba_linux-64.sh
 
 load-sysenv
 
-mk-env cray-cuda-mpich-perlmutter
+mk-env intel-syc-mpich-arcticus
 if fix-sysversions
 then
     return 1
 fi
-mk-cctbx cuda build hot update
+mk-cctbx sycl build hot update
 patch-dispatcher alcf
 
 cat > ${ALCC_CCTBX_ROOT}/activate.sh << EOF
@@ -36,7 +36,7 @@ source ${ALCC_CCTBX_ROOT}/opt/site/alcf_arcticus.sh
 load-sysenv
 activate
 
-export CUDA_LAUNCH_BLOCKING=1
-export SIT_DATA=\${OVERWRITE_SIT_DATA:-\$NERSC_SIT_DATA}:\$SIT_DATA
-export SIT_PSDM_DATA=\${OVERWRITE_SIT_PSDM_DATA:-\$NERSC_SIT_PSDM_DATA}
+#export CUDA_LAUNCH_BLOCKING=1
+#export SIT_DATA=\${OVERWRITE_SIT_DATA:-\$NERSC_SIT_DATA}:\$SIT_DATA
+#export SIT_PSDM_DATA=\${OVERWRITE_SIT_PSDM_DATA:-\$NERSC_SIT_PSDM_DATA}
 EOF
