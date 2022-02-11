63c0785505c6dcb1eefb7530ac27c1858f46d14a
Servesh Muralidharan <1692420+servesh@users.noreply.github.com>
2022-02-11 16:08:11 -0600

    Load sys env when updating modules

--- cctbx/arcticus_update_modules.sh
+++ cctbx/arcticus_update_modules.sh
@@ -9,6 +9,11 @@ source ${ALCC_CCTBX_ROOT}/utilities_alcf.sh
 source ${ALCC_CCTBX_SITE_ARCTICUS}
 
 echo "*"
+echo "* Load Sysenv (see log.load_sysenv)"
+echo "*"
+load-sysenv > log.load_sysenv 2>&1
+
+echo "*"
 echo "* Update modules directory (see log.update)"
 echo "*"
 mk-cctbx kokkos-alcf hot update > log.update 2>&1
